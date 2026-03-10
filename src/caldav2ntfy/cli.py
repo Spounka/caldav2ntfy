5afcef5e3f662a4dc5b5d69664a8fbc4d1763cd8
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2026-03-11 00:47:40 +0100

    refactor(cli): split main function into testable parts

--- src/caldav2ntfy/cli.py
+++ src/caldav2ntfy/cli.py
@@ -1,32 +1,44 @@
 #!/bin/env python3
 
 import logging
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from typing import Any
 
 from caldav2ntfy.config import APP_NAME, find_config, load_config, setup_logger
 
 config: dict = {}
 
 
-def main():
-    setup_logger()
-    logger = logging.getLogger(APP_NAME)
-
+def parse_arguments() -> Namespace:
     parser = ArgumentParser()
 
     parser.add_argument("--config", help="Path to custom configuration file")
     parser.add_argument("--ntfy-server", help="Override ntfy url")
     parser.add_argument("--token", help="Override ntfy token")
     parser.add_argument("--topic", help="Override ntfy topic")
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def override_config_from_cli_params(
+    args: Namespace, defaults: dict[str, Any]
+) -> dict[str, str]:
+    if args.ntfy_server:
+        defaults["ntfy"]["server"] = args.ntfy_server
+    if args.token:
+        defaults["ntfy"]["token"] = args.token
+    if args.topic:
+        defaults["ntfy"]["topic"] = args.topic
+    return defaults
+
+
+def main():
+    setup_logger()
+    logger = logging.getLogger(APP_NAME)
+
+    args = parse_arguments()
 
     config_path = find_config(args.config)
 
     config = load_config(config_path) if config_path else {}
-    if args.ntfy_server:
-        config["ntfy"]["server"] = args.ntfy_server
-    if args.token:
-        config["ntfy"]["token"] = args.token
-    if args.topic:
-        config["ntfy"]["topic"] = args.topic
+    config = override_config_from_cli_params(args, config)
     logger.info(f"Loaded Config with the following params: {config}")
